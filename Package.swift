6a56b5a5dc7ad08dd6001768a3f2144b99942f03
mr-swifter <103502437+mr-swifter@users.noreply.github.com>
2023-12-29 13:46:38 +0200

    rename

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "FrostflakeDllKit",
+            name: "Frostflake",
             targets: ["FrostflakeDllKit"]),
     ],
     targets: [
