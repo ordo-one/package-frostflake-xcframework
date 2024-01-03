401daf2f99a2112fc8b4ca87f22bb5cafff8b5b4
mr-swifter <103502437+mr-swifter@users.noreply.github.com>
2024-01-03 18:26:35 +0200

    chore: [sc-455] use different XCFrameworks for different platforms

--- Package.swift
+++ Package.swift
@@ -3,6 +3,31 @@
 
 import PackageDescription
 
+let xcframeworks: [String: String] = [
+    "macos-arm64": "a12edc35269ad9a2c4a136ed9c8caf296a628878934fb745ef4467e4ff83c3f5",
+    "linux-aarch64": "b0f41ea7d12e877bec6aab596178f973988e7c335aaeca129f6b6080d9b517da",
+    "linux-x86_64": "bcde8f81babd503688d391d336c1b7727fc008c87055260f741e761ca74ec8ac"
+]
+
+let platform: String = {
+#if os(macOS) && arch(arm64)
+    return "macos-arm64"
+#elseif os(Linux) && arch(arm64)
+    return "linux-aarch64"
+#elseif os(Linux) && arch(x86_64)
+    return "linux-x86_64"
+#else
+    preconditionFailure("Unsupported architecture")
+#endif
+}()
+
+let checksum: String = {
+    guard let checksum = xcframeworks[platform] else {
+        preconditionFailure("No checksum for platform \(platform)")
+    }
+    return checksum
+}()
+
 let package = Package(
     name: "package-frostflake-xcframework",
     platforms: [
@@ -22,8 +47,8 @@ let package = Package(
         ),
         .binaryTarget(
             name: "FrostflakeKit",
-            url: "https://github.com/ordo-one/package-frostflake/raw/xcframework/FrostflakeKit.xcframework.zip",
-            checksum: "6d41161ce83dc2cd917ee0ca53451990532cccc0710c2d0fe11c69ebf7e97ade"
+            url: "https://github.com/ordo-one/package-frostflake/raw/xcframework/xcframeworks/\(platform)/FrostflakeKit.xcframework.zip",
+            checksum: checksum
         )
     ]
 )
