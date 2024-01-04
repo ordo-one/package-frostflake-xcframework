721cce82974087ee17b45e8aa82dd09e47d67e70
mr-swifter <103502437+mr-swifter@users.noreply.github.com>
2024-01-04 13:53:53 +0200

    feat(patch): add macos-x86_64

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let xcframeworks: [String: String] = [
     "macos-arm64": "a12edc35269ad9a2c4a136ed9c8caf296a628878934fb745ef4467e4ff83c3f5",
+    "macos-x86_64": "61916fc4e86d7bb19562fbeb69bc341a9594e3af3613e3d216d3e4e15658dccb",
     "linux-aarch64": "b0f41ea7d12e877bec6aab596178f973988e7c335aaeca129f6b6080d9b517da",
     "linux-x86_64": "bcde8f81babd503688d391d336c1b7727fc008c87055260f741e761ca74ec8ac"
 ]
@@ -12,6 +13,8 @@ let xcframeworks: [String: String] = [
 let platform: String = {
 #if os(macOS) && arch(arm64)
     return "macos-arm64"
+#elseif os(macOS) && arch(x86_64)
+    return "macos-x86_64"
 #elseif os(Linux) && arch(arm64)
     return "linux-aarch64"
 #elseif os(Linux) && arch(x86_64)
